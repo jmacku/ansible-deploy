122f9ea13f0900b6001f2c29e62ad43ffa6bc989
Jaroslav Macků <jaroslav.macku@kardi-ai.com>
2026-06-24 10:03:37 +0200

    Update Uptime Kuma Nginx configurations for status page, standardize SSL paths, and fix vault script typo

--- ansible/vault-setup.sh
+++ ansible/vault-setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export ANSIBLE_VAULT_PASSWORD_FILE=${PWD}/valut-env.sh
+export ANSIBLE_VAULT_PASSWORD_FILE=${PWD}/vault-env.sh
 
 echo "Provide ANSIBLE_VAULT_PASSWORD: "
-read -s ANSIBLE_VAULT_PASSWORD && export ANSIBLE_VAULT_PASSWORD
+read -sr ANSIBLE_VAULT_PASSWORD && export ANSIBLE_VAULT_PASSWORD
